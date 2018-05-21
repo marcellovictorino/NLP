4c67f8c27e00bcd3513ce31283bfca3bfde81f52
Marcello Victorino <mvictorino@sugarlandtx.gov>
2018-05-21 17:37:22 -0500

    Twitter api key and tokens for Data Streaming

---
+++ Twitter Sentiment Analysis.py
@@ -0,0 +1,51 @@
+
+
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+from twitterAPI import *
+# import sentiment_mod as s # replace with the actual module name
+
+# consumer key, consumer secret, access token, access secret. All imported from twitterAPI module
+
+class listener(StreamListener):
+    
+    def on_data(self, data):
+        all_data = json.loads(data)
+        tweet = all_data["text"]
+        # Testing: filtering specific words of interest
+        if 'traffic' in tweet.lower():
+            print(tweet)
+        else:
+            print('Nothing about traffic ...')
+		# sentiment_value, confidence = s.sentiment(tweet)
+		# print(tweet, sentiment_value, confidence)
+        print(tweet,'\n')
+
+		# if confidence*100 >= 80:
+		# 	output = open("twitter-out.txt","a")
+		# 	output.write(sentiment_value)
+		# 	output.write('\n')
+		# 	output.close()
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, listener())
+
+#########################################
+# Can only use one of the following filters
+
+### 1) Look for specific words or Hashtags
+# twitterStream.filter(track=["traffic"])
+# twitterStream.filter(track=['#SugarLand', '#COSL'])
+
+### 2) Only look for specific user tweets
+# twitterStream.filter(follow=[''])
+
+### 3) Only look for tweet from a specific location (bounding box)
+twitterStream.filter(locations=[-95.696438,29.540022,-95.576973,29.654943]) # Sugar Land Area
---
+++ twitterAPI.py
@@ -0,0 +1,6 @@
+# Contains all the Keys/Secretes/Tokens necessary to stream Twitter API
+
+ckey = 'w3sg5aL17TnfyCmk2Yu7nascB'
+csecret = 'nt5FhbRkCnZ1aO4fhxikv6nBwgcIffY6QxoKDGmI5BTjwY5mlV'
+atoken = '544360990-cwPZ0MpJruU2XNMS9xJiH7R7PxpvWBkGY92b1kUN'
+asecret = 'creX8BhajIpQmsDppM343wYlbLtnrIhk9pHhoECiGcR1N'
